608a68842389d7fba27fb93013e1216b7bb75d8a
RogerSanchezTorres <rogerst2002@gmail.com>
2023-11-06 12:46:37 +0100

    estilo

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,271 +1,9 @@
-<!--<div id="navegador">
-    <ul class="nav">
-        <li>
-            <a href="#">Electricidad</a>
-            <ul>
-                <div class="desplegable-electricidad">
-                    <div id="mecanismos">
-                        <a href="{{ route('products.index') }}" class="titulo-desplegable">MECANISMOS</a>
-                        <li class="submenus"><a href="#">Bjc</a></li>
-                        <li class="submenus"><a href="#">Efapel</a></li>
-                        <li class="submenus"><a href="#">Mecanismos para exterior</a></li>
-                        <li class="submenus"> <a href="#">Simon</a></li>
-                    </div>
-                </div>
-            </ul>
-            <ul>
-                <div id="proteccion">
-                    <a href="#" class="titulo-desplegable">PROTECCIÓN</a>
-                    <li class="submenus"><a href="#">Famatel</a></li>
-                    <li class="submenus"><a href="#">Hyundai</a></li>
-                    <li class="submenus"><a href="#">Sistemas de control y protección</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="iluminacion">
-                    <a href="#" class="titulo-desplegable">ILUMINACIÓN</a>
-                    <li class="submenus"><a href="#">Laes</a></li>
-                    <li class="submenus"><a href="#">Luxlight</a></li>
-                    <li class="submenus"><a href="#">Novolux</a> </li>
-                    <li class="submenus"><a href="#">Secom</a></li>
-                    <li class="submenus"><a href="#">Yld</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="redes">
-                    <a href="#" class="titulo-desplegable">REDES Y TELECOMUNICACIONES</a>
-                    <li class="submenus"><a href="#">Fermax</a></li>
-                    <li class="submenus"><a href="#">Openetics</a></li>
-                    <li class="submenus"><a href="#">Televés</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="cables">
-                    <a href="#" class="titulo-desplegable">CABLES Y CANALIZACIONES</a>
-                    <li class="submenus"><a href="#">Efapel</a></li>
-                    <li class="submenus"><a href="#">Miguelez</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="ventilacion">
-                    <a href="#" class="titulo-desplegable">VENTILACIÓN</a>
-                    <li class="submenus"><a href="#">Novovent</a></li>
-                    <li class="submenus"><a href="#">S&P</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="material-instalacion">
-                    <a href="#" class="titulo-desplegable">MATERIAL DE INSTALACIÓN</a>
-                    <li class="submenus"><a href="#">Cajas derivación</a></li>
-                    <li class="submenus"><a href="#">Cajas para mecanismos</a></li>
-                    <li class="submenus"><a href="#">Pequeño material</a></li>
-                </div>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Fontanería</a>
-            <ul>
-                <div class="desplegable-fontaneria">
-                    <div id="jardin">
-                        <a href="#" class="titulo-desplegable">JARDÍN Y PISCINA</a>
-                        <li class="submenus"><a href="#">Accesorios piscina</a></li>
-                        <li class="submenus"><a href="#">Electrobombas</a></li>
-                        <li class="submenus"><a href="#">Filtración</a></li>
-                        <li class="submenus"><a href="#">Químicos piscina</a></li>
-                        <li class="submenus"><a href="#">Riego</a></li>
-                        <li class="submenus"><a href="#">Tubo PVC presión y accesorios</a></li>
-                    </div>
-                </div>
-            </ul>
-            <ul>
-                <div id="tratamiento-agua">
-                    <a href="#" class="titulo-desplegable">TRATAMIENTOS AGUA</a>
-                    <li class="submenus"><a href="#">Antical</a></li>
-                    <li class="submenus"><a href="#">Descalcificadores</a></li>
-                    <li class="submenus"><a href="#">Filtración</a></li>
-                    <li class="submenus"><a href="#">Osmosis</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="calefaccion">
-                    <a href="#" class="titulo-desplegable">CALEFACCIÓN Y ACS</a>
-                    <li class="submenus"><a href="#">Acumuladores</a></li>
-                    <li class="submenus"><a href="#">Bombas</a></li>
-                    <li class="submenus"><a href="#">Calderas de gasoil</a></li>
-                    <li class="submenus"><a href="#">Calderas murales</a></li>
-                    <li class="submenus"><a href="#">Calentadores</a></li>
-                    <li class="submenus"><a href="#">Radiadores</a></li>
-                    <li class="submenus"><a href="#">Suelo radiante</a></li>
-                    <li class="submenus"><a href="#">Termos eléctricos</a></li>
-                    <li class="submenus"><a href="#">Termostatos</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="accesorios-instalacion">
-                    <a href="#" class="titulo-desplegable">ACCESORIOS INSTALACIÓN</a>
-                    <li class="submenus"><a href="#">Aislamientos</a></li>
-                    <li class="submenus"><a href="#">Batería agua</a></li>
-                    <li class="submenus"><a href="#">Fijaciones</a></li>
-                    <li class="submenus"><a href="#">Flexos</a></li>
-                    <li class="submenus"><a href="#">Racorería</a></li>
-                    <li class="submenus"><a href="#">Sifones</a></li>
-                    <li class="submenus"><a href="#">Tubería PVC y accesorios</a></li>
-                    <li class="submenus"><a href="#">Tubo cobre y accesorios</a></li>
-                    <li class="submenus"><a href="#">Tubo multicapa y accesorios</a></li>
-                    <li class="submenus"><a href="#">Tubo PEX-A y accesorios</a></li>
-                    <li class="submenus"><a href="#">Tubo polietileno y accesorios</a></li>
-                    <li class="submenus"><a href="#">Válvula</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="energias-renovables">
-                    <a href="#" class="titulo-desplegable">ENERGÍAS RENOVABLES</a>
-                    <li class="submenus"><a href="#">Aerotérmia</a></li>
-                    <li class="submenus"><a href="#">Energía solar térmica</a></li>
-                </div>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Gas</a>
-            <ul>
-                <div class="desplegable-gas">
-                    <div id="tuberia">
-                        <a href="#" class="titulo-desplegable">TUBERÍA Y ACC. MULTICAPA GAS</a>
-                        <li class="submenus"><a href="#">Accesorios multicapa gas</a></li>
-                        <li class="submenus"><a href="#">Tubo multicapa gas</a></li>
-                    </div>
-                </div>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Construcción</a>
-            <ul>
-                <div class="desplegable-construccion">
-                    <div id="impermeabilizacion">
-                        <a href="#" class="titulo-desplegable">IMPERMEABILIZACIÓN</a>
-                        <li class="submenus"><a href="#">Butilo</a></li>
-                        <li class="submenus"><a href="#">Pinturas impermeabilizantes</a></li>
-                        <li class="submenus"><a href="#">Telas asfálticas</a></li>
-                    </div>
-                </div>
-            </ul>
-            <ul>
-                <div id="tabiqueria">
-                    <a href="#" class="titulo-desplegable">TABIQUERÍA SECA</a>
-                    <li class="submenus"><a href="#">Accesorios</a></li>
-                    <li class="submenus"><a href="#">Aislantes</a></li>
-                    <li class="submenus"><a href="#">Perfilería y tornillería</a></li>
-                    <li class="submenus"><a href="#">Placas</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="productos-quimicos">
-                    <a href="#" class="titulo-desplegable">PRODUCTOS QUÍMICOS</a>
-                    <li class="submenus"><a href="#">Adhesivos y selladores</a></li>
-                    <li class="submenus"><a href="#">Anclajes</a></li>
-                    <li class="submenus"><a href="#">Espumas</a></li>
-                    <li class="submenus"><a href="#">Masillas</a></li>
-                    <li class="submenus"><a href="#">Químicos construcción</a></li>
-                    <li class="submenus"><a href="#">Siliconas</a></li>
-                </div>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Cerámica</a>
-            <ul>
-                <div class="desplegable-ceramica">
-                    <div id="interior">
-                        <a href="#" class="titulo-desplegable">INTERIOR</a>
-                        <li class="submenus"><a href="#">Aparici</a></li>
-                        <li class="submenus"><a href="#">Apavisa</a></li>
-                        <li class="submenus"><a href="#">Azulejos Benadresa</a></li>
-                        <li class="submenus"><a href="#">Ceramat</a></li>
-                        <li class="submenus"><a href="#">Dune</a></li>
-                        <li class="submenus"><a href="#">Hisbalit</a></li>
-                        <li class="submenus"><a href="#">Keros</a></li>
-                        <li class="submenus"><a href="#">Natucer</a></li>
-                        <li class="submenus"><a href="#">Togama</a></li>
-                        <li class="submenus"><a href="#">Vilar Albaro</a></li>
-                    </div>
-                </div>
-            </ul>
-            <ul>
-                <div id="exterior">
-                    <a href="#" class="titulo-desplegable">EXTERIOR</a>
-                    <li class="submenus"><a href="#">Azuliber</a></li>
-                    <li class="submenus"><a href="#">Cerámica Ferrés</a></li>
-                    <li class="submenus"><a href="#">Cerámica Calaf</a></li>
-                    <li class="submenus"><a href="#">Cirt piedra</a></li>
-                    <li class="submenus"><a href="#">Greco Gres</a></li>
-                    <li class="submenus"><a href="#">Gres de Aragón</a></li>
-                    <li class="submenus"><a href="#">Keraben</a></li>
-                    <li class="submenus"><a href="#">Resigres</a></li>
-                </div>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Sanitario y grifería</a>
-            <ul>
-                <div class="desplegable-sanitario">
-                    <div id="accesorios-baño">
-                        <a href="#" class="titulo-desplegable">ACCESORIOS DE BAÑO</a>
-                        <li class="submenus"><a href="#">Mediterránea del baño</a></li>
-                        <li class="submenus"><a href="#">Nofer</a></li>
-                    </div>
-                </div>
-            </ul>
-            <ul>
-                <div id="griferia">
-                    <a href="#" class="titulo-desplegable">GRIFERÍA</a>
-                    <li class="submenus"><a href="#">Clever</a></li>
-                    <li class="submenus"><a href="#">Grohe</a></li>
-                    <li class="submenus"><a href="#">Idral</a></li>
-                    <li class="submenus"><a href="#">Imex</a></li>
-                    <li class="submenus"><a href="#">Presto</a></li>
-                    <li class="submenus"><a href="#">Ramón Soler</a></li>
-                    <li class="submenus"><a href="#">Roca</a></li>
-                </div>
-            </ul>
-            <ul>
-                <div id="sanitario">
-                    <a href="#" class="titulo-desplegable">SANITARIO</a>
-                    <li class="submenus"><a href="#">Bañeras</a></li>
-                    <li class="submenus"><a href="#">Inodoros</a></li>
-                    <li class="submenus"><a href="#">Lavabos</a></li>
-                    <li class="submenus"><a href="#">Platos de ducha</a></li>
-                    <li class="submenus"><a href="#">Recambios</a></li>
-                </div>
-            </ul>
-        </li>
-        <li>
-            <a href="#">Ropa laboral y seguridad</a>
-        </li>
-        <li>
-            <a href="#">Herramientas</a>
-            <ul>
-                <div class="desplegable-herramientas">
-                    <div id="herramientas-fontaneria">
-                        <a href="#" class="titulo-desplegable">HERRAMIENTAS FONTANERÍA</a>
-                        <li class="submenus"><a href="#">Apriete</a></li>
-                        <li class="submenus"><a href="#">Prensado</a></li>
-                    </div>
-                </div>
-            </ul>
-            <ul>
-                <div id="herramientas-construccion">
-                    <a href="#" class="titulo-desplegable">HERRAMIENTAS CONSTRUCCIÓN</a>
-                    <li class="submenus"><a href="#">Corte cerámica</a></li>
-                    <li class="submenus"><a href="#">Discos de diamante</a></li>
-                    <li class="submenus"><a href="#">Herramientas varias construcción</a></li>
-                    <li class="submenus"><a href="#">Nivelación, medición y marcaje</a></li>
-                </div>
-            </ul>
-        </li>
-    </ul>
-</div>-->
-
 <div id="navegador">
-    <ul class="nav">
+    <div class="menu-icon" id="menu-icon">
+        &#9776;
+    </div>
+    <ul class="nav" id="nav">
+        <span id="close-icon" class="close-icon">&times;</span>
         @foreach(\App\Models\MainCategory::all() as $mainCategory)
         <li>
             <a href="{{ route('products.showByMainCategory', ['mainCategoryId' => $mainCategory->id]) }}" class="titulo-desplegable">{{ $mainCategory->nombre }}</a>
@@ -273,3 +11,22 @@
         @endforeach
     </ul>
 </div>
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuIcon = document.getElementById('menu-icon');
+        const nav = document.getElementById('nav');
+        const closeIcon = document.getElementById('close-icon');
+
+        menuIcon.addEventListener('click', function() {
+            nav.classList.toggle('active');
+            document.body.classList.toggle('nav-active');
+        });
+
+        closeIcon.addEventListener('click', function() {
+            nav.classList.remove('active');
+            document.body.classList.remove('nav-active');
+        });
+    });
+</script>
