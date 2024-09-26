59c0767e7339fca99e081287196384afe9b2d356
RogerSanchezTorres <rogerst2002@gmail.com>
2024-09-26 11:22:12 +0200

    prueba3

--- resources/views/products/show_by_main_category.blade.php
+++ resources/views/products/show_by_main_category.blade.php
@@ -8,7 +8,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <!--<link rel="stylesheet" href="{{ asset('/style/products/pagination.css') }}">-->
+    <link rel="stylesheet" href="{{ asset('/style/products/pagination.css') }}">
 
 </head>
 
