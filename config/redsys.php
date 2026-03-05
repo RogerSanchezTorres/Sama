ac7abb7ccfcd3a236946db65944f02de26eb1d07
RogerSanchezTorres <rogerst2002@gmail.com>
2026-03-05 11:40:48 +0100

    test

--- config/redsys.php
+++ config/redsys.php
@@ -6,6 +6,6 @@ return [
     'url_ko'                => env('REDSYS_URL_KO','http://127.0.0.1:8000/redsys/ko'),
     'merchantcode'          => env('REDSYS_MERCHANT_CODE','48295810'),
     'terminal'              => env('REDSYS_TERMINAL','001'),
-    'enviroment'            => env('REDSYS_ENVIROMENT','live'),
+    'enviroment'            => env('REDSYS_ENVIROMENT','test'),
     'tradename'             => env('REDSYS_TRADENAME','SubministresSama'),
 ];
