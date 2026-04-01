da6a2b1529f5386e53d848ed515641f34475a527
RogerSanchezTorres <rogerst2002@gmail.com>
2026-04-01 10:42:03 +0200

    mail

--- app/Http/Controllers/RedsysController.php
+++ app/Http/Controllers/RedsysController.php
@@ -145,6 +145,18 @@ class RedsysController extends Controller
                 'authorization_code' => $params['Ds_AuthorisationCode']
             ]);
 
+            // 🔔 EMAIL A ADMIN
+            Mail::to(config('mail.from.address')) // o un email fijo tipo empresa@gmail.com
+                ->send(new OrderAdminNotification($order));
+
+            // 🔔 EMAIL AL USUARIO
+            $user = User::find($order->user_id);
+
+            if ($user && $user->email) {
+                Mail::to($user->email)
+                    ->send(new OrderConfirmed($order));
+            }
+
             $cart = Cart::where('user_id', $order->user_id)->with('product')->get();
 
             foreach ($cart as $item) {
